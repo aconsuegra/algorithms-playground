cb035734804b9b30b9212ff7c37cbb6cc8380ba9
Antonio J. Consuegra <anconsue@microsoft.com>
2017-06-04 16:58:51 +0200

    checkstyle and travis

--- src/main/java/com/aconsuegra/algorithms/BestTimeBuyStocks.java
+++ src/main/java/com/aconsuegra/algorithms/BestTimeBuyStocks.java
@@ -22,7 +22,7 @@ public class BestTimeBuyStocks {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for(int i = 0; i < prices.length; i++) {
+        for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minPrice) {
                 minPrice = prices[i];
             } else if (prices[i] - minPrice > maxProfit) {
