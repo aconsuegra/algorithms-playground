1253a8cda11ef8760937e53598828800c0949ee2
Antonio J. Consuegra <anconsue@microsoft.com>
2017-06-15 19:32:38 +0200

    Add data structure utils in Kotlin

--- src/main/kotlin/com/aconsuegra/algorithms/KValidateBinarySearchTree.kt
+++ src/main/kotlin/com/aconsuegra/algorithms/KValidateBinarySearchTree.kt
@@ -1,29 +1,29 @@
 package com.aconsuegra.algorithms
 
-import com.aconsuegra.datastructure.BinaryTreeNode
+import com.aconsuegra.datastructure.KBinaryTreeNode
 
 /**
  * Implement a function to check if a binary tree is a binary search tree
  */
 class KValidateBinarySearchTree {
 
-    fun isBST(node: BinaryTreeNode<Int>): Boolean = isBST(node, null, null)
+    fun isBST(node: KBinaryTreeNode<Int>): Boolean = isBST(node, null, null)
 
-    private fun isBST(node: BinaryTreeNode<Int>?, min: Int?, max: Int?): Boolean {
+    private fun isBST(node: KBinaryTreeNode<Int>?, min: Int?, max: Int?): Boolean {
         if (node == null) {
             return true
         }
         if (minValue(node, min) || maxValue(node, max)) {
             return false
         }
-        if (!isBST(node.left, min, node.`val`) || !isBST(node.right, node.`val`, max)) {
+        if (!isBST(node.left, min, node.data) || !isBST(node.right, node.data, max)) {
             return false
         }
         return true
     }
 
-    fun minValue(node: BinaryTreeNode<Int>, min: Int?) = min != null && node.`val` <= min
+    fun minValue(node: KBinaryTreeNode<Int>, min: Int?) = min != null && node.data <= min
 
-    fun maxValue(node: BinaryTreeNode<Int>, max: Int?) = max != null && node.`val` > max
+    fun maxValue(node: KBinaryTreeNode<Int>, max: Int?) = max != null && node.data > max
 
 }
