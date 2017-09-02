cf7e90fb4fa049127634b6fb82f3b2c4c75b926b
Antonio J. Consuegra <anconsue@microsoft.com>
2017-09-02 18:33:18 +0200

    3 more algos in Kotlin

---
+++ src/main/kotlin/com/aconsuegra/algorithms/KLongestCommonPrefix.kt
@@ -0,0 +1,28 @@
+package com.aconsuegra.algorithms
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ */
+class KLongestCommonPrefix {
+
+    fun longestCommonPrefix(input: Array<String>) : String {
+        if (input.isEmpty()) {
+            return ""
+        }
+        var result = input[0]
+        for (index in 1 until input.size) {
+            result = findCommonPrefix(result, input[index])
+        }
+        return result
+    }
+
+    private fun findCommonPrefix(prefix: String, str: String) : String {
+        val sb = StringBuilder()
+        val length = Math.min(prefix.length, str.length)
+        (0..length - 1)
+                .takeWhile { prefix[it] == str[it] }
+                .forEach { sb.append(prefix[it]) }
+        return sb.toString()
+    }
+
+}
